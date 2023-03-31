e5c8c2ba6b451e4f537b81788ff91b59f1566f5e
Dmytro2V <op2vin@gmail.com>
2023-03-31 04:04:27 +0300

    phase0

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,3 +1,6 @@
+import {useContext} from 'react';
+import {useTheme} from '../../context/ThemeContext.js'
+
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
@@ -5,12 +8,15 @@ import './Greenhouse.css';
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
-function Greenhouse() {
 
+
+function Greenhouse() {
+  const {themeName} = useTheme();
+  const image = themeName === 'day'? dayImage: nightImage;
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={image}
             alt='greenhouse' 
       />
       <LightSwitch />
